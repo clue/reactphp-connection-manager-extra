e670410dba61201be932dc562ce12a30bb3904d2
Christian Lück <christian@lueck.tv>
2013-06-24 09:14:43 +0200

    Fix canceling timeout

--- ConnectionManager/Extra/ConnectionManagerTimeout.php
+++ ConnectionManager/Extra/ConnectionManagerTimeout.php
@@ -11,36 +11,36 @@ class ConnectionManagerTimeout implements ConnectorInterface
     private $connectionManager;
     private $loop;
     private $timeout;
-    
+
     public function __construct(ConnectorInterface $connectionManager, LoopInterface $loop, $timeout)
     {
         $this->connectionManager = $connectionManager;
         $this->loop = $loop;
         $this->timeout = $timeout;
     }
-    
+
     public function create($host, $port)
     {
         $deferred = new Deferred();
         $timedout = false;
-        
+
         $tid = $this->loop->addTimer($this->timeout, function() use ($deferred, &$timedout) {
             $deferred->reject(new Exception('Connection attempt timed out'));
             $timedout = true;
             // TODO: find a proper way to actually cancel the connection
         });
-        
+
         $loop = $this->loop;
         $this->connectionManager->create($host, $port)->then(function ($connection) use ($tid, $loop, &$timedout, $deferred) {
             if ($timedout) {
                 // connection successfully established but timeout already expired => close successful connection
                 $connection->end();
             } else {
-                $loop->removeTimeout($tid);
+                $loop->cancelTimer($tid);
                 $deferred->resolve($connection);
             }
         }, function ($error) use ($loop, $tid) {
-            $loop->removeTimeout($tid);
+            $loop->cancelTimer($tid);
             throw $error;
         });
         return $deferred->promise();
